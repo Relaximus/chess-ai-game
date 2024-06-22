6a06743352765eaa23ca527c7d0c6e2070d849d6
relaximus <info@chentsov.de>
2024-06-22 22:28:43 +0200

    adding history to the request

--- src/main/java/com/relaximus/chessengine/AiMoveService.java
+++ src/main/java/com/relaximus/chessengine/AiMoveService.java
@@ -1,16 +1,25 @@
 package com.relaximus.chessengine;
 
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toCollection;
 import static org.springframework.util.StringUtils.hasLength;
 
 @Service
@@ -19,18 +28,28 @@ public class AiMoveService {
     @Autowired
     OllamaChatClient chatClient;
 
-    String suggestMove(String fen, String moves, String check) {
+    Tuple2<String, String> suggestMove(String fen, String moves, String check, List<String> history) {
         var systemMessage = new SystemMessage("""
-                 You represent a professional chess player.
-                 You are playing for Black side.
-                 As an input you will receive the FEN of the game, possible moves to chose from and indicator if its check situation or not.
-                 Restore the board setup according to the input.
-                 Pick up one best move from available moves, it has to be most effective move to win the game.
-                 Place the move option inside of tag <mark><mark/>.
-                 If you don't know the answer just return an empty <mark></mark>
+                Let's play chess.
+                I'll give you current FEN of the game and the list of possible moves at the desk at the moment.
+                Could you suggest the next move for the Black side.
+                First of all, check if any of your figures under attack and try to safe them with the next move.
+                At the same time if there is a possibility to eat white figures without danger to be attack, do it!
+                If there are multiple white figures available for attack, chose the most important one for attacking!
+                Also I let you know if there is check situation you can use for taking decisions.
+                In general, it has to be most effective move to win the game, you can use known plays from history and common chess strategies to win.
+                Place the move option inside of tag <mark><mark/>.
+                If you don't know the answer just return an empty <mark></mark>
                 """);
-        var userMessage = new PromptTemplate("""
-                Her are the FEN of the chess game.
+        List<Message> historyOfChat = history.stream()
+                .gather(Gatherers.windowFixed(2))
+                .flatMap(strings -> Stream.of(
+                        new UserMessage(strings.get(0)),
+                        new AssistantMessage(strings.get(1))))
+                .collect(toCollection(() -> new ArrayList<>(List.of(systemMessage))));
+
+        var lastUserMessage = new PromptTemplate("""
+                So her is the FEN of the chess game.
                              
                 {fen}
                              
@@ -42,15 +61,11 @@ public class AiMoveService {
                           
                 {check}
                              
-                Give me the best move with this input""")
-                .createMessage(Map.of(
-                        "fen", fen,
-                        "moves", moves,
-                        "check", hasLength(check) ? check + " side has a check situation." : "There is no check situation at the moment"
-                ));
+                Give me the best move with this input""").createMessage(Map.of("fen", fen, "moves", moves, "check", hasLength(check) ? check + " side has a check situation." : "There is no check situation at the moment"));
 
-        ChatResponse response = chatClient.call(new Prompt(List.of(userMessage, systemMessage)));
+        historyOfChat.add(lastUserMessage);
+        ChatResponse response = chatClient.call(new Prompt(historyOfChat));
 
-        return response.getResult().getOutput().getContent();
+        return Tuples.of(lastUserMessage.getContent(), response.getResult().getOutput().getContent());
     }
 }
--- src/main/java/com/relaximus/chessengine/MoveController.java
+++ src/main/java/com/relaximus/chessengine/MoveController.java
@@ -1,10 +1,16 @@
 package com.relaximus.chessengine;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.util.function.Tuple2;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Collections.*;
+import static java.util.Objects.*;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -13,8 +19,16 @@ public class MoveController {
     @Autowired
     AiMoveService moveService;
 
-    @GetMapping("/move")
-    String move(@RequestParam(required = false) String fen, @RequestParam(required = false) String moves, @RequestParam(required = false) String check) {
-        return moveService.suggestMove(fen, moves, check);
+    @PostMapping("/move")
+    MoveResponse move(@RequestBody MoveQuery query) {
+        Tuple2<String, String> result = moveService.suggestMove(query.fen(), query.moves().toString(),
+                query.check(), requireNonNullElse(query.history(), emptyList()));
+        return new MoveResponse(result.getT1(), result.getT2());
+    }
+
+    record MoveResponse(String question, String answer) {
+    }
+
+    record MoveQuery(String fen, List<String> moves, String check, List<String> history) {
     }
 }
